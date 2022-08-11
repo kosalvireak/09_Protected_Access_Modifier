ffd720cdfc8077f52540baae9d0042f78f748ba0
Kosalvireak Khoeun <virakvary@gmail.com>
2022-08-11 13:10:02 +0700

    add read only property

---
+++ Lesson09Exercises2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson09Exercises2
+{
+    class Supermarket
+    {
+        private string strSupermarketName = "Nice Supermarket";
+        protected static int intCustomerCount;
+        protected static float fltTotalSales;
+        public string SupermarketName //b
+        {
+            get { return strSupermarketName; }
+        }
+
+
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
